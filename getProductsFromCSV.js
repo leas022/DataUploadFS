af6dc7fa962d8c1e71d535a70dad01451a894c4c
Luca <leas022@aucklanduni.ac.nz>
2023-08-20 19:23:02 +1200

    Add working code

---
+++ getProductsFromCSV.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const csvParser = require("csv-parser");
+
+function getProductsFromCSV() {
+
+    const results = [];
+
+    try {
+        const csvData = fs.readFileSync('input.csv', 'utf-8');
+        const rows = csvData.split('\n');
+
+        for (let i = 1; i < rows.length; i++) {
+            const row = rows[i].trim();
+            if (row) {
+                const columns = row.split(',');
+
+                const object = {
+                    id: parseInt(columns[0]),
+                    price: parseFloat(columns[1]),
+                    publisher: columns[2],
+                    genre: columns[3],
+                    playerCount: parseInt(columns[4]),
+                    playTime: parseInt(columns[5]),
+                    title: columns[6],
+                    description: columns[7],
+                    images: [`${columns[8]}`, `${columns[9]}`, `${columns[10]}`],
+                    releasedYear: parseInt(columns[11]),
+                };
+                results.push(object);
+            }
+        }
+
+        // Now 'results' contains an array of objects representing the CSV data
+    } catch (error) {
+        console.error('Error reading or processing the CSV file:', error);
+    }
+
+    return results;
+
+}
+
+module.exports = getProductsFromCSV;
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 // Firestore Admin
 
-const admin = require("firebase-admin");
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-const serviceAccount = require("serviceAccountKey.json");
+const serviceAccount = require('./serviceAccountKey.json');
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+async function addProductToFirestore(product) {
+    const boardgameDocRef = boardgamesColRef.doc(`${product.id}`);
+    await boardgameDocRef.set(product);
+}
+
+const db = getFirestore();
+const boardgamesColRef = db.collection("products").doc("boardgames").collection("items");
+
+const getProductsFromCSV = require("./getProductsFromCSV");
+const products = getProductsFromCSV();
+
+products.forEach((product) => {
+    addProductToFirestore(product).then(() => {
+        console.log(`Success! ${product.id}`);
+    }).catch((error) => {
+        console.log(`Error! ${product.id}`);
+        console.error(error);
+    });
 });
